e0622947299400ca879cad80aa37af8372b1c337
main <RuslanAtasev1923@gmail.com>
2023-10-05 23:35:58 +0300

    KS-2-1

---
+++ src/main/kotlin/lesson_2/lesson_2_task_1.kt
@@ -0,0 +1,12 @@
+package lesson_2
+
+fun main() {
+
+    val studentAssessmentOne = 3f
+    val studentAssessmentTwo = 4
+    val studentAssessmentThree = 3
+    val studentAssessmentFour = 5
+
+    println((studentAssessmentOne + studentAssessmentTwo + studentAssessmentThree + studentAssessmentFour) / 4 )
+
+}
