a6e7f53e6fecbc476c5e4e861158f5df88d4fcff
main <RuslanAtasev1923@gmail.com>
2023-10-06 21:29:10 +0300

    KS-1-3 fix

--- src/main/kotlin/lesson_1/lesson_1_task_3.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_3.kt
@@ -2,13 +2,13 @@ package lesson_1
 
 fun main() {
 
-    val year = 1961
-    var hour = 9
-    var minute = 55
+    val year = "1961"
+    var hour = "09"
+    var minute = "55"
     print(year)
-    println(" 0" + hour + ":" + minute)
-    hour = 10
-    minute = 55
-    print("" + hour + ":" + minute)
+    println(" $hour:$minute")
+    hour = "10"
+    minute = "55"
+    print("$hour:$minute")
 
 }
