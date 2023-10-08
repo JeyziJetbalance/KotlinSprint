f7733a0a55395344f2a8b39054bbadf9872bf5a6
main <RuslanAtasev1923@gmail.com>
2023-10-08 20:28:31 +0300

    KS-2-4 fix

--- src/main/kotlin/lesson_2/lesson_2_task_4.kt
+++ src/main/kotlin/lesson_2/lesson_2_task_4.kt
@@ -5,12 +5,11 @@ fun main() {
     val crystalOre = 7
     val ironOre = 11
 
-    val baff = 120
-    val baffCrystalOre = crystalOre * baff / 100
-    val baffIronOre = ironOre * baff / 100
-
-    println(baffCrystalOre - crystalOre)
-    println(baffIronOre - ironOre)
+    val baff = 20
+    val baffCrystalOre =( crystalOre * baff ) / 100
+    val baffIronOre = (ironOre * baff) / 100
+    println(baffCrystalOre)
+    println(baffIronOre)
 
 }
 
