ecba92bcc5d8096b8404d79d2b6673dbe94d4866
main <RuslanAtasev1923@gmail.com>
2023-10-09 00:07:27 +0300

    KS-3-2

---
+++ src/main/kotlin/lesson_3/lesson_3_task_2.kt
@@ -0,0 +1,15 @@
+package lesson_3
+
+fun main() {
+
+    var userAge = 20
+    val userName = "Татьяна"
+    var userSurname = "Андреева"
+    val userPatronymic = "Сергеевна"
+    println("[$userSurname $userName $userPatronymic, $userAge]")
+
+    userAge = 22
+    userSurname = "Сидорова"
+    println("[$userSurname $userName $userPatronymic, $userAge]")
+
+}
