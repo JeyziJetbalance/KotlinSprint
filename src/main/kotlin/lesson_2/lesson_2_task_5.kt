6bee70868fb502ed742eadad7dfcc97dc8c2cefb
main <RuslanAtasev1923@gmail.com>
2023-10-13 17:58:08 +0300

    KS-2-5 fix

--- src/main/kotlin/lesson_2/lesson_2_task_5.kt
+++ src/main/kotlin/lesson_2/lesson_2_task_5.kt
@@ -7,7 +7,6 @@ fun main() {
     val depositAmount = 70000
     val interestRate = 16.7
     val depositTime = 20
-    val accrualPeriods = 1
     val totalAmountDeposit = depositAmount * (1 + interestRate/100).pow(depositTime)
     println(String.format("%.3f", totalAmountDeposit))
 
