8e843f476d23d51af4714c8a78297d0a7ca07980
main <RuslanAtasev1923@gmail.com>
2023-10-13 16:22:25 +0300

    KS-1-5 fix

--- src/main/kotlin/lesson_1/lesson_1_task_5.kt
+++ src/main/kotlin/lesson_1/lesson_1_task_5.kt
@@ -2,17 +2,11 @@ package lesson_1
 
 fun main() {
 
-//    var second: Int = 6480
-//    val minute: Int = second / 60 - 60
-//    val hour: Int = second / 3600
-//    second = second % 60
-//    println("0" + hour + ":" + minute + ":" + "0" + second)
-//    println(String.format(""))
-
     var second = 6480
     var minute = second / 60
     val hour = second / 3600
     minute %= 60
     second %= 60
     println(String.format("%02d:%02d:%02d", hour, minute, second))
+
 }
