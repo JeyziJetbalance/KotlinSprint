eb0b57188ff4af8268837e502c97a92053777bd6
main <RuslanAtasev1923@gmail.com>
2023-10-14 11:43:47 +0300

    KS-3-4 fix

--- src/main/kotlin/lesson_3/lesson_3_task_4.kt
+++ src/main/kotlin/lesson_3/lesson_3_task_4.kt
@@ -2,11 +2,11 @@ fun main() {
 
     var whereFrom = "E2"
     var where = "E4"
-    var strokeNumber = 1
-    println("[$whereFrom-$where;$strokeNumber]")
+    var strokeNumber = 0
+    println("$whereFrom-$where;$strokeNumber")
 
     whereFrom = "D2"
     where = "D3"
     strokeNumber += 1
-    println("[$whereFrom-$where;$strokeNumber]")
+    println("$whereFrom-$where;$strokeNumber")
 }
