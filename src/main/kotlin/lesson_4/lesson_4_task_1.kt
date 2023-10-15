0acf35059cbe8da1526614f0d54fa73ed8f16731
main <RuslanAtasev1923@gmail.com>
2023-10-16 00:33:47 +0300

    KS-4-1

---
+++ src/main/kotlin/lesson_4/lesson_4_task_1.kt
@@ -0,0 +1,10 @@
+fun main() {
+
+    val today = 13
+    val tomorrow = 13 - 4
+    val availabilityToday = today < 13
+    val availabilityTomorrow = tomorrow < 13
+    println("Доступность столиков на сегодня: $availabilityToday")
+    println("Доступность столиков на завтра: $availabilityTomorrow")
+
+}
