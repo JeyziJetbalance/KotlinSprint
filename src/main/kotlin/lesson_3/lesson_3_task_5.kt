ba3b4ca51fe5c9ca675a331865f0661c3ac86bc7
main <RuslanAtasev1923@gmail.com>
2023-10-15 22:02:12 +0300

    KS-3-5

---
+++ src/main/kotlin/lesson_3/lesson_3_task_5.kt
@@ -0,0 +1,13 @@
+package lesson_3
+fun main() {
+
+    val string = "D2-D4;0"
+    val parts  = string.split("-", ";") //
+    val whereFrom = parts[0]
+    val where = parts[1]
+    val strokeNumber = parts[2]
+
+    println("Откуда: $whereFrom")
+    println("Куда: $where")
+    println("Номер хода: $strokeNumber")
+}
