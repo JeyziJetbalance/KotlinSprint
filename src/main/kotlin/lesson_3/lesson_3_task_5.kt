d807d45d44100584e54cd83eab6d9a2a1eb6378b
main <RuslanAtasev1923@gmail.com>
2023-10-15 22:11:34 +0300

    KS-3-5 fix

--- src/main/kotlin/lesson_3/lesson_3_task_5.kt
+++ src/main/kotlin/lesson_3/lesson_3_task_5.kt
@@ -1,8 +1,9 @@
 package lesson_3
+
 fun main() {
 
     val string = "D2-D4;0"
-    val parts  = string.split("-", ";") //
+    val parts = string.split("-", ";") //
     val whereFrom = parts[0]
     val where = parts[1]
     val strokeNumber = parts[2]
