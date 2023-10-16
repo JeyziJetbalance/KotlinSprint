1e3b3afac123bca891be410323aef9cb7fd61f77
main <RuslanAtasev1923@gmail.com>
2023-10-16 12:59:29 +0300

    KS-4-1 fix

--- src/main/kotlin/lesson_4/lesson_4_task_1.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_1.kt
@@ -1,12 +1,14 @@
 fun main() {
 
     val today = 13
-    val tomorrow = 13 - 4
-    val availabilityToday = today < 13
-    val availabilityTomorrow = tomorrow < 13
+    val tomorrow = 9
+    val availabilityToday = today < NUMBER_OF_TABLES
+    val availabilityTomorrow = tomorrow < NUMBER_OF_TABLES
     println(
         "Доступность столиков на сегодня: $availabilityToday\n" +
                 "Доступность столиков на завтра: $availabilityTomorrow"
     )
 
 }
+
+const val NUMBER_OF_TABLES = 13
