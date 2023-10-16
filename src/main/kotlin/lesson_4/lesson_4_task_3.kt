1904c36b2a4df48027bcd3f0cc2fd21b7f5b2b1b
main <RuslanAtasev1923@gmail.com>
2023-10-16 13:15:27 +0300

    KS-4-3 fix

--- src/main/kotlin/lesson_4/lesson_4_task_3.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_3.kt
@@ -1,17 +1,17 @@
 fun main() {
 
-    val weather = true
-    val outdoorAwning = true
-    val airHumidity = 20
-    val season = "зима"
-
-    val weatherToday = true
-    val outdoorAwningToday = true
+    val isSunny = true
+    val isAwningOpen = true
     val airHumidityToday = 20
     val seasonToday = "зима"
 
     val verificationResult =
-        (weather == weatherToday) && (outdoorAwning == outdoorAwningToday) && (airHumidity == airHumidityToday) && (seasonToday != season)
+        (SUNNY == isSunny) && (AWNING_OPEN == isAwningOpen) && (AIR_HUMIDITY == airHumidityToday) && (seasonToday != season)
 
     println("Благоприятные ли условия сейчас для роста бобовых? $verificationResult")
 }
+
+const val SUNNY = true
+const val AWNING_OPEN = true
+const val AIR_HUMIDITY = 20
+const val season = "зима"
