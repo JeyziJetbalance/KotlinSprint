4643b2e2563d99edf4e6e6cd4a3da415d8e9ba35
main <RuslanAtasev1923@gmail.com>
2023-10-18 22:00:41 +0300

    KS-5-1

--- src/main/kotlin/lesson_5/lesson_5_task_1.kt
+++ src/main/kotlin/lesson_5/lesson_5_task_1.kt
@@ -1,14 +1,2 @@
-fun main() {
+package lesson_5
 
-    val firstTerm = 5
-    val secondTerm = 8
-    val resultAddition = firstTerm + secondTerm
-    println("Напишите результат математического примера на сложение: $firstTerm + $secondTerm")
-    val answer = readLine()!!.toInt()
-
-    val resultVerification = if (answer == resultAddition) "Добро пожаловать!"
-    else "Доступ запрещен."
-
-    println(resultVerification)
-
-}
