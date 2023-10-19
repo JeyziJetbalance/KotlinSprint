4336faacb2132e1a4d1e04985c812ca31602ca74
main <RuslanAtasev1923@gmail.com>
2023-10-19 21:37:20 +0300

    KS-4-5 fix

--- src/main/kotlin/lesson_4/lesson_4_task_5.kt
+++ src/main/kotlin/lesson_4/lesson_4_task_5.kt
@@ -1,14 +1,14 @@
 fun main() {
 
-    val isShipHaveDamage = readLine()!!.toBoolean()
-    val numberOfCrew = readLine()!!.toInt()
-    val numberBoxesProvisions = readLine()!!.toInt()
-    val isWeatherFovourable = readLine()!!.toBoolean()
+    val isShipHaveDamage = readln().toBoolean()
+    val numberOfCrew = readln().toInt()
+    val numberBoxesProvisions = readln().toInt()
+    val isWeatherFovourable = readln().toBoolean()
 
     val resultText = if ((isShipHaveDamage == IS_SHIP_HAVE_DAMAGE) && ((numberOfCrew >= MIN_NUMBER_OF_CREW) &&
-                (numberOfCrew <= MAX_NUMBER_OF_CREW)) && (numberBoxesProvisions >= MIN_BOXES_OF_PROVISIONS)) "Корабль готов к долгосрочному плаванию."
+                (numberOfCrew <= MAX_NUMBER_OF_CREW)) && (numberBoxesProvisions > MIN_BOXES_OF_PROVISIONS)) "Корабль готов к долгосрочному плаванию."
     else if ((isShipHaveDamage != IS_SHIP_HAVE_DAMAGE) && (numberOfCrew == MAX_NUMBER_OF_CREW) &&
-        (numberBoxesProvisions >= MIN_BOXES_OF_PROVISIONS) && (isWeatherFovourable == IS_WEATHER_FAVOURABLE_ALTERNATIVE)) "Корабль может отплыть с незначительными ограничениями."
+        (numberBoxesProvisions > MIN_BOXES_OF_PROVISIONS) && (isWeatherFovourable == IS_WEATHER_FAVOURABLE_ALTERNATIVE)) "Корабль может отплыть с незначительными ограничениями."
     else "Корабль не готов к отплытию."
 
     println(resultText)
