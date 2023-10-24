571a889b04ec79758731cb609bf2526a51719503
main <RuslanAtasev1923@gmail.com>
2023-10-24 23:25:32 +0300

    KS-6-5 fix

--- src/main/kotlin/lesson_6/lesson_6_task_5.kt
+++ src/main/kotlin/lesson_6/lesson_6_task_5.kt
@@ -8,38 +8,46 @@ fun main() {
     var counterBot = 0
     var counter = 0
 
-    while (counter == 0) {
+    while (counter < 3) {
 
         print("Введите логин: ")
-        var loginVerification = readln()
+        val loginVerification = readln()
         print("Введите пароль: ")
         val passwordVerification = readln()
 
-        if ((loginVerification != login) || (passwordVerification != password)) println("Данные не верны.")
-        else counter++
-
-        while (counterBot != 3) {
-
-            val firstNumber = (1..9).random()
-            val secondNumber = (1..9).random()
-            val result = firstNumber + secondNumber
-            print("Решите пример: $firstNumber + $secondNumber = ")
-            val resultVerification = readln().toInt()
-
-            if (counterBot == 2) {
-                println("Отказано в доступе.")
-                return
-            }
-
-            if (result == resultVerification) {
-                println("Добро пожаловать!")
-                return
-            } else {
-                println("Попробуйте еще раз.")
-                counterBot++
-            }
-
+        if ((loginVerification == login) && (passwordVerification == password)) {
+            println("Пройдите проверку на бота.")
+            break
+        } else {
+            counter++
+            println("Данные не верны. Введите заного логин и пароль. При неправильном вводе 3 раза доступ отклонен")
         }
 
     }
+
+    if (counter == 3) return
+
+    while (counterBot != 3) {
+
+        val firstNumber = (1..9).random()
+        val secondNumber = (1..9).random()
+        val result = firstNumber + secondNumber
+        print("Решите пример: $firstNumber + $secondNumber = ")
+        val resultVerification = readln().toInt()
+
+        if (counterBot == 2) {
+            println("Отказано в доступе.")
+            return
+        }
+
+        if (result == resultVerification) {
+            println("Добро пожаловать!")
+            return
+        } else {
+            println("Попробуйте еще раз.")
+            counterBot++
+        }
+
+    }
+
 }
